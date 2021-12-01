9cf6c8395fcfae7969525425a2654f0f27b35e8a
Kyle Worthington <tallonrain@tallonra.in>
2021-12-01 12:45:28 -0800

    day 1 part 1

---
+++ main.py
@@ -0,0 +1,10 @@
+sonardata = open('input', 'r')
+n = 0
+count = 0
+for line in sonardata:
+    lineint = int(line)
+    if n < lineint and n != 0:
+        count += 1
+    n = lineint
+
+print(count)
