91bf8d5ffc0ef1f4a307fafe6a6aa5e19b43bb46
Kyle Worthington <tallonrain@tallonra.in>
2021-12-01 17:58:21 -0800

    day 1 part 2 improved

--- main.py
+++ main.py
@@ -5,15 +5,11 @@ for i in file:
 
 n = 0
 count = 0
-sum = 0
-while n < len(sonardata) and n != 1998:
-    previous = sum
-    sum = 0
-    a = [sonardata[n], sonardata[n + 1], sonardata[n + 2]]
-    for i in a:
-        sum = sum + i
-    if previous < sum and previous != 0:
+evaluation = 0
+while n < len(sonardata) - 2:
+    previous = evaluation
+    evaluation = sonardata[n] + sonardata[n + 1] + sonardata[n + 2]
+    if previous < evaluation and previous != 0:
         count += 1
     n += 1
-
 print("count:", count)
