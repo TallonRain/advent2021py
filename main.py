7e3c3f1ef3082a5fde4bedb34d664866a1d06d82
Kyle Worthington <tallonrain@tallonra.in>
2021-12-01 17:08:16 -0800

    day 1 part 2

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
-sonardata = open('input', 'r')
+file = open('input', 'r')
+sonardata = []
+for i in file:
+    sonardata.append(int(i))
+
 n = 0
 count = 0
-for line in sonardata:
-    lineint = int(line)
-    if n < lineint and n != 0:
+sum = 0
+while n < len(sonardata) and n != 1998:
+    previous = sum
+    sum = 0
+    a = [sonardata[n], sonardata[n + 1], sonardata[n + 2]]
+    for i in a:
+        sum = sum + i
+    if previous < sum and previous != 0:
         count += 1
-    n = lineint
+    n += 1
 
-print(count)
+print("count:", count)
